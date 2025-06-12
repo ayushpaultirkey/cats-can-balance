02d7f7b9bfb8741a23e0a88d7e693a28414e0312
Ayush Paul Tirkey <49755887+ayushpaultirkey@users.noreply.github.com>
2025-06-12 11:25:46 +0530

    Updated BalanceGame.tsx

--- src/client/BalanceGame.tsx
+++ src/client/BalanceGame.tsx
@@ -2,5 +2,12 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 
 export const BalanceGame: React.FC = () => {
-    
+  return <>
+      <div>
+        <label>Cats can balance</label>
+        <button>Left</button>
+        <button>Right</button>
+        <label>Score:</label>
+      </div>
+  </>
 }
