65c6eb4f4c2868e3d891610c6d463776c8a89d52
Ayush Paul Tirkey <49755887+ayushpaultirkey@users.noreply.github.com>
2025-06-14 10:16:44 +0530

    Updated main.tsx

--- src/devvit/main.tsx
+++ src/devvit/main.tsx
@@ -92,7 +92,7 @@ Devvit.addCustomPostType({
       async onMessage(message, webView) {
         switch (message.type) {
           case 'setScore':
-            await context.redis.set(`counter_${context.postId}`, message.data.newCounter.toString());
+            await context.redis.set(`counter_${context.postId}`, message.data.newScore.toString());
             setCounter(message.data.newCounter);
             break;
         }
