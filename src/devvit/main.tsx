8d0d965c3606e631433e3b16c6e6ca2fcd4a0997
Ayush Paul Tirkey <49755887+ayushpaultirkey@users.noreply.github.com>
2025-06-14 11:40:01 +0530

    Updated main.tsx

--- src/devvit/main.tsx
+++ src/devvit/main.tsx
@@ -86,7 +86,11 @@ Devvit.addCustomPostType({
       let currentScore = 0;
       try {
         const userId = await context.reddit.getCurrentUsername();
-        currentScore = await context.redis.zScore('scores', 'user');
+        //currentScore = await context.redis.zScore('scores', 'user');
+        await context.redis.zAdd(
+          'scores',
+          { member: 'user', score: 37 },
+        );
       } catch (error) {
         console.error(error);
       }
