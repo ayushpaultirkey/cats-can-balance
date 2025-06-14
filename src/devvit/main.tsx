a628f20536fb05ea517553f6f864763095ff94b6
Ayush Paul Tirkey <49755887+ayushpaultirkey@users.noreply.github.com>
2025-06-14 08:40:46 +0530

    Updated main.tsx

--- src/devvit/main.tsx
+++ src/devvit/main.tsx
@@ -1,72 +1,75 @@
-import { Devvit, Post } from '@devvit/public-api';
+import { Devvit, Post } from "@devvit/public-api";
 
-import '../server/index';
-import { defineConfig } from '@devvit/server';
-import { postConfigNew } from '../server/core/post';
+import "../server/index";
+import { defineConfig } from "@devvit/server";
 
 defineConfig({
-  name: 'Cats can balance',
-  entry: 'index.html',
-  height: 'tall',
-  menu: { enable: false },
+    name: "Cats can balance",
+    entry: "index.html",
+    height: "tall",
+    menu: { enable: false },
 });
 
-export const Preview: Devvit.BlockComponent<{ text?: string }> = ({ text = 'Loading...' }) => {
-  return (
-    <zstack width={'100%'} height={'100%'} alignment="center middle">
-      <vstack width={'100%'} height={'100%'} alignment="center middle">
-        <image
-          url="loading.gif"
-          description="Loading..."
-          height={'140px'}
-          width={'140px'}
-          imageHeight={'240px'}
-          imageWidth={'240px'}
-        />
-        <spacer size="small" />
-        <text maxWidth={`80%`} size="large" weight="bold" alignment="center middle" wrap>
-          {text}
-        </text>
-      </vstack>
-    </zstack>
-  );
+export const Preview: Devvit.BlockComponent<{ text?: string }> = ({
+    text = "Loading...",
+}) => {
+    return (
+        <zstack width={"100%"} height={"100%"} alignment="center middle">
+            <vstack width={"100%"} height={"100%"} alignment="center middle">
+                <image
+                    url="loading.gif"
+                    description="Loading..."
+                    height={"140px"}
+                    width={"140px"}
+                    imageHeight={"240px"}
+                    imageWidth={"240px"}
+                />
+                <spacer size="small" />
+                <text
+                    maxWidth={`80%`}
+                    size="large"
+                    weight="bold"
+                    alignment="center middle"
+                    wrap
+                >
+                    {text}
+                </text>
+            </vstack>
+        </zstack>
+    );
 };
 
 // TODO: Remove this when defineConfig allows webhooks before post creation
 Devvit.addMenuItem({
-  // Please update as you work on your idea!
-  label: '[Cats can balance]: New Game',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async (_event, context) => {
-    const { reddit, ui } = context;
+    // Please update as you work on your idea!
+    label: "[Cats can balance]: New Game",
+    location: "subreddit",
+    forUserType: "moderator",
+    onPress: async (_event, context) => {
+        const { reddit, ui } = context;
 
-    let post: Post | undefined;
-    try {
-      const subreddit = await reddit.getCurrentSubreddit();
-      post = await reddit.submitPost({
-        // Title of the post. You'll want to update!
-        title: 'Cats can balance !',
-        subredditName: subreddit.name,
-        preview: <Preview />,
-      });
-      await postConfigNew({
-        redis: context.redis,
-        postId: post.id,
-      });
-      ui.showToast({ text: 'Created post!' });
-      ui.navigateTo(post.url);
-    } catch (error) {
-      if (post) {
-        await post.remove(false);
-      }
-      if (error instanceof Error) {
-        ui.showToast({ text: `Error creating post: ${error.message}` });
-      } else {
-        ui.showToast({ text: 'Error creating post!' });
-      }
-    }
-  },
+        let post: Post | undefined;
+        try {
+            const subreddit = await reddit.getCurrentSubreddit();
+            post = await reddit.submitPost({
+                // Title of the post. You'll want to update!
+                title: "Cats can balance !",
+                subredditName: subreddit.name,
+                preview: <Preview />,
+            });
+            ui.showToast({ text: "Created post!" });
+            ui.navigateTo(post.url);
+        } catch (error) {
+            if (post) {
+                await post.remove(false);
+            }
+            if (error instanceof Error) {
+                ui.showToast({ text: `Error creating post: ${error.message}` });
+            } else {
+                ui.showToast({ text: "Error creating post!" });
+            }
+        }
+    },
 });
 
 export default Devvit;
